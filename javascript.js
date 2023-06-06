575506fcd839c6e0d3c19ff3e2b6f7352a6f1406
dev.ankur <ankurbhati9027@gmail.com>
2023-06-06 19:53:33 +0530

    top picks added

--- javascript.js
+++ javascript.js
@@ -1,18 +1,18 @@
-const url = 'https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '08ea465befmsh8ad2171b9c00af1p14f9eajsnfe886a65f2d1',
-		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-	}
-};
-async function checkApi()
-{try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-};
-checkApi();
+// const url = 'https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '08ea465befmsh8ad2171b9c00af1p14f9eajsnfe886a65f2d1',
+// 		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+// 	}
+// };
+// async function checkApi()
+// {try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.json();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+// };
+// checkApi();
